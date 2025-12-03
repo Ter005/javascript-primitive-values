911f0ea2b2c9d116d41f86fb3a6d9d9bcfffd15b
Chaiyan <chaiyan.p@hotmail.com>
2025-12-03 23:35:41 +0700

    ex-5 completed

--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,8 @@
 //Exercise #5: Shopping and Discount
 
 // Start coding here
+let productPrice = 27000
+let productDiscount = productPrice*0.25
+let productPriceNet =productPrice-productDiscount
+console.log(productPriceNet);
+
